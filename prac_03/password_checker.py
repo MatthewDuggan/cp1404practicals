137bfc77950364bdd655ad847e10519ad55c900b
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-23 12:40:42 +1000

    Refactor password program to use functions

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,7 +1,19 @@
-password = input("input your password: ")
-MIN_LENGTH = 10
-if len(password) >= MIN_LENGTH:
+def main():
+    get_password()
+    print_asterisks(password)
+
+
+def get_password():
+    MIN_LENGTH = 10
+    password = input("input your password: ")
+    while len(password) < MIN_LENGTH:
+        print("The password does not reach the required length")
+        password = input("input your password: ")
+    return password
+
+
+def print_asterisks(password):
     for char in password:
-        print("*",end="")
-else:
-    print("The password does not reach the required length")
+        print("*", end="")
+
+main()
