fedaaf7e76b9430171f5589b523e89553a8233c1
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-23 12:07:14 +1000

    Add broken score file to git

---
+++ prac_01/broken_score.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+# score = float(input("Enter score: "))
+# if 90 <= score <= 100:
+#     print("Excellent")
+# elif 50 <= score < 90:
+#     print("Pass")
+# elif 0 <= score <50:
+#     print ("Bad")
+# else:
+#     print("Invalid Input")
+
+x = 5
+y = 10
+if x < 10:
+    if y > 10 and x > 5:
+        print("A")
+else:
+    print("B")
