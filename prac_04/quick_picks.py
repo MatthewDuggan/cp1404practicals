ce4239cfb921c97d24c345760574dbb8d1697d4d
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-29 23:03:10 +1000

    Refactored into main() function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,17 +4,22 @@ NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
 
-number_of_quick_picks = int(input("How many quick picks would you like?: "))
 
-for picks in range(0, number_of_quick_picks):
-    quick_pick = []
-    for lines in range(0, NUMBERS_PER_LINE):
-        new_number = random.randint(MINIMUM,MAXIMUM)
-        while new_number in quick_pick:
-            new_number = random.randint(MINIMUM, MAXIMUM)
-        quick_pick.append(new_number)
-        quick_pick.sort()
-    print(" ".join("{:2}".format(number) for number in quick_pick))
+def main():
+    number_of_quick_picks = int(input("How many quick picks would you like?: "))
+
+    for picks in range(0, number_of_quick_picks):
+        quick_pick = []
+        for lines in range(0, NUMBERS_PER_LINE):
+            new_number = random.randint(MINIMUM,MAXIMUM)
+            while new_number in quick_pick:
+                new_number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(new_number)
+            quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
 
 
 
