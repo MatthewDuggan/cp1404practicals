c5ad82746201cfb2201cf05063248d442ca95e72
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-29 23:01:01 +1000

    Complete quick pick program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,15 +6,17 @@ MAXIMUM = 45
 
 number_of_quick_picks = int(input("How many quick picks would you like?: "))
 
-quick_pick = []
 for picks in range(0, number_of_quick_picks):
+    quick_pick = []
     for lines in range(0, NUMBERS_PER_LINE):
         new_number = random.randint(MINIMUM,MAXIMUM)
         while new_number in quick_pick:
             new_number = random.randint(MINIMUM, MAXIMUM)
         quick_pick.append(new_number)
         quick_pick.sort()
-    print(quick_pick)
+    print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
 
 
 
