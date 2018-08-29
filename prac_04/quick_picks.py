16c21c1cc2d94f69d13e9c75dc5dfcec625257bd
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-08-29 22:48:04 +1000

    Add generation and sorting of random numbers but does not start a new array for a new line

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,5 +1,25 @@
-NUMBERS = []
-for i in range(1, 46):
-    NUMBERS.append(i)
-print(NUMBERS)
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
 number_of_quick_picks = int(input("How many quick picks would you like?: "))
+
+quick_pick = []
+for picks in range(0, number_of_quick_picks):
+    for lines in range(0, NUMBERS_PER_LINE):
+        new_number = random.randint(MINIMUM,MAXIMUM)
+        while new_number in quick_pick:
+            new_number = random.randint(MINIMUM, MAXIMUM)
+        quick_pick.append(new_number)
+        quick_pick.sort()
+    print(quick_pick)
+
+
+
+
+
+
+
+
