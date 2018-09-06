27645c88a2aa2d4f52657f7d3d4b66daa8667b1e
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-09-06 11:43:48 +1000

    Enter dictionary of 10 hex colours and their codes

---
+++ prac_05/hexadecimal_colours.py
@@ -0,0 +1,17 @@
+COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "aquamarine1": "#7fffd4",
+           "azure1": "f0ffff", "beige": "#f5f5dc", "bisque1": "ffe4c4", "black": "#000000", "blue1": "#0000ff",
+           "BlueViolet": "#8a2be2"}
+# print(STATE_NAMES)
+
+print(len(COLOURS))
+
+# state = input("Enter short state: ")
+# while state != "":
+#     if state.upper() in STATE_NAMES:
+#         print("{:3} is {}".format(state, STATE_NAMES[state.upper()]))
+#     else:
+#         print("Invalid short state")
+#     state = input("Enter short state: ")
+#
+# for state in STATE_NAMES:
+#     print("{:3} is {}".format(state, STATE_NAMES[state.upper()]))
