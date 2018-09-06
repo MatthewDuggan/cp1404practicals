5dc5040667cbc70921d3a4689ca98668c670b821
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-09-06 12:17:34 +1000

    Complete word counts exercise

--- prac_05/word_count.py
+++ prac_05/word_count.py
@@ -1,8 +1,12 @@
-text = "this is very nice and is good"
+text = input("Enter a string to count the words: ")
 word_counts = {}
 words = text.split()
 for word in words:
     frequency = word_counts.get(word, 0)
     word_counts[word] = frequency + 1
 
-print(word_counts)
+longest_word_length = max(len(word) for word in words)
+
+alphabetical_words = sorted(word_counts)
+for word in alphabetical_words:
+    print("{:{}} : {}".format(word, longest_word_length, word_counts[word]))
