bbb8aeb55fee01383da987ae3d509685ec458481
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-09-06 11:53:14 +1000

    Complete corrected exercise

--- prac_05/hexadecimal_colours.py
+++ prac_05/hexadecimal_colours.py
@@ -1,3 +1,5 @@
+
+
 COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "aquamarine1": "#7fffd4",
            "azure1": "f0ffff", "beige": "#f5f5dc", "bisque1": "ffe4c4", "black": "#000000", "blue1": "#0000ff",
            "BlueViolet": "#8a2be2"}
@@ -5,7 +7,7 @@ COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "
 colour_name = input("Enter colour name: ")
 while colour_name != "":
     if colour_name in COLOURS:
-        print("{} is {}".format(colour_name, COLOURS(colour_name)))
+        print("{} is {}".format(colour_name, COLOURS[colour_name]))
     else:
         print("Invalid code")
     colour_name = input("Enter colour name: ")
