4519d1e2298c2ccf7e2859cc005e7bb0887c3d12
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-09-06 11:27:28 +1000

    Complete exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,13 +5,17 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
 state = input("Enter short state: ")
 while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
+    if state.upper() in STATE_NAMES:
+        print("{:3} is {}".format(state,STATE_NAMES[state.upper()]))
     else:
         print("Invalid short state")
     state = input("Enter short state: ")
+
+for state in STATE_NAMES:
+    print("{:3} is {}".format(state, STATE_NAMES[state.upper()]))
