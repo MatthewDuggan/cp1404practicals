f0e1b781c9a70b7132966f6957c8c1b6ac9ce173
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-09-06 11:50:25 +1000

    Add while loop that is not working

--- prac_05/hexadecimal_colours.py
+++ prac_05/hexadecimal_colours.py
@@ -1,17 +1,11 @@
 COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "aquamarine1": "#7fffd4",
            "azure1": "f0ffff", "beige": "#f5f5dc", "bisque1": "ffe4c4", "black": "#000000", "blue1": "#0000ff",
            "BlueViolet": "#8a2be2"}
-# print(STATE_NAMES)
 
-print(len(COLOURS))
-
-# state = input("Enter short state: ")
-# while state != "":
-#     if state.upper() in STATE_NAMES:
-#         print("{:3} is {}".format(state, STATE_NAMES[state.upper()]))
-#     else:
-#         print("Invalid short state")
-#     state = input("Enter short state: ")
-#
-# for state in STATE_NAMES:
-#     print("{:3} is {}".format(state, STATE_NAMES[state.upper()]))
+colour_name = input("Enter colour name: ")
+while colour_name != "":
+    if colour_name in COLOURS:
+        print("{} is {}".format(colour_name, COLOURS(colour_name)))
+    else:
+        print("Invalid code")
+    colour_name = input("Enter colour name: ")
