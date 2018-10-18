8a011720e80f41a78195015129bb533c5591231c
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-10-18 12:28:26 +1000

    Refactor silver service taxi test into a main() function

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -1,6 +1,11 @@
 from silver_service_taxi import SilverServiceTaxi
 
-fancy_taxi = SilverServiceTaxi("Sydney Taxi", 100, 2)
-fancy_taxi.drive(18)
-print(fancy_taxi)
-print(fancy_taxi.get_fare())
+
+def main():
+    fancy_taxi = SilverServiceTaxi("Sydney Taxi", 100, 2)
+    fancy_taxi.drive(18)
+    print(fancy_taxi)
+    print(fancy_taxi.get_fare())
+
+
+main()
---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,5 @@
+from car import Car
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
