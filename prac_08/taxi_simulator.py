40471e2d7e42eece78ffea8b13580ed0e3712737
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-10-18 12:54:21 +1000

    Complete taxi simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -4,6 +4,7 @@ from silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+
 def main():
     total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
@@ -18,12 +19,19 @@ def main():
             taxi_choice = int(input("Choose Taxi:"))
             current_taxi = taxis[taxi_choice]
         elif menu_choice == "d":
-            print("selected drive")
+            current_taxi.start_fare()
+            distance_to_drive = float(input("Drive how far?"))
+            current_taxi.drive(distance_to_drive)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost ${}".format(current_taxi.name, trip_cost))
+            total_cost += trip_cost
         else:
             print("Invalid option")
         print("Bill to date: ${}".format(total_cost))
-        menu_choice = input(">>>")
-    print("Total trip cost:",format(total_cost))
+        print(MENU)
+        menu_choice = input(">>>").lower()
+    print("Total trip cost:", format(total_cost))
+    print("Taxis are now:")
     display_taxis(taxis)
 
 
@@ -31,5 +39,6 @@ def main():
 def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print("{} - {}".format(i, str(taxi)))
+
 
 main()
