f4fc30b0c6c0f9a3a4a3212f135865119cda52c5
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-10-18 12:39:00 +1000

    Create menu of taxi simulator program

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -2,4 +2,19 @@ from car import Car
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+
 def main():
+    total_cost = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print("q)uit, c)hoose taxi, d)rive")
+    menu_choice = imput(">>>").lower
+    while menu_choice != "q":
+        if menu_choice = "c":
+            print("selected choose taxis")
+        elif menu_choice="d":
+            print("selected drive")
+        else:
+            print("Invalid option")
+        print("Bill to date: ${}".format(total_cost))
+
