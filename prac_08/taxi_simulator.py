092f47f4ef2877f3ca74251f3cd326a9fdb4378d
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-10-25 09:33:51 +1000

    Complete TODOs in Modifications task

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -6,6 +6,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    current_taxi = ""
     total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
@@ -19,12 +20,15 @@ def main():
             taxi_choice = int(input("Choose Taxi:"))
             current_taxi = taxis[taxi_choice]
         elif menu_choice == "d":
-            current_taxi.start_fare()
-            distance_to_drive = float(input("Drive how far?"))
-            current_taxi.drive(distance_to_drive)
-            trip_cost = current_taxi.get_fare()
-            print("Your {} trip cost ${}".format(current_taxi.name, trip_cost))
-            total_cost += trip_cost
+            if current_taxi == "":
+                print("Please choose a taxi first")
+            else:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far?"))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print("Your {} trip cost ${}".format(current_taxi.name, trip_cost))
+                total_cost += trip_cost
         else:
             print("Invalid option")
         print("Bill to date: ${}".format(total_cost))
@@ -33,7 +37,6 @@ def main():
     print("Total trip cost:", format(total_cost))
     print("Taxis are now:")
     display_taxis(taxis)
-
 
 
 def display_taxis(taxis):
