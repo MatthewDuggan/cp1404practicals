356927d02f3f8887d9f0e3c154a750c767e80e69
MatthewDuggan <matthew.duggan@my.jcu.edu.au>
2018-10-25 12:34:15 +1000

    Refactor version of of files sorting into a miain function with call

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,18 +1,22 @@
 import os
 
-os.chdir("FilesToSort") # change to correct dir
-extensions = []
-for filename in os.listdir("."):
-    if os.path.isdir(filename):
-        continue
-    extension = filename.split(".")[-1]
-    if extension not in extensions:
-        extensions.append(extension)
-        try:
-            os.mkdir(extension)
-        except FileExistsError:
-            pass
-    os.rename(filename, "{}/{}".format(extension, filename))
-print(extensions)
+
+def main():
 
 
+    os.chdir("FilesToSort")  # change to correct dir
+    extensions = []
+    for filename in os.listdir("."):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split(".")[-1]
+        if extension not in extensions:
+            extensions.append(extension)
+            try:
+                os.mkdir(extension)
+            except FileExistsError:
+                pass
+        os.rename(filename, "{}/{}".format(extension, filename))
+    print(extensions)
+
+main()
